263ec2124b17b2b8d64aff9d17421680909fe233
0785675371 <appolondiho@gmail.com>
2022-07-06 09:35:10 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AsideSection from "./messages-list";
+
+function App() {
+  return (
+   <>
+   <AsideSection />
+   <section className="messages-view"></section>
+   </>
+  );
+}
+export default App;
